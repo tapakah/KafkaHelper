5c938509c2112a812b2c9d14a4403977416cd6ab
ib-skundiy <ib-skundiy@lime-systems.com>
2022-12-27 16:07:25 +0200

    add tooltips

--- KafkaHelpers/Forms/MessageDetailForm.cs
+++ KafkaHelpers/Forms/MessageDetailForm.cs
@@ -10,68 +10,68 @@ using System.Windows.Forms;
 
 namespace KafkaHelpers
 {
-        public partial class MessageDetailForm : Form
+    public partial class MessageDetailForm : Form
+    {
+        private MessageDetailEntity _entiry = new MessageDetailEntity();
+        public MessageDetailEntity Entity { get { return _entiry; } }
+
+        public MessageDetailForm()
         {
-                private MessageDetailEntity _entiry = new MessageDetailEntity();
-                public MessageDetailEntity Entity { get { return _entiry; } }
-
-                public MessageDetailForm()
-                {
-                        InitializeComponent();
-                }
-
-
-                private void MessageDetailForm_Load(object sender, EventArgs e)
-                {
-                        tbId.Text = _entiry.Id.ToString();
-                        tbTopic.Text = _entiry.Topic.ToString();
-                        tbKey.Text = _entiry.KeyString;
-                        lbSize.Text = RowFormatter.FormatSize(System.Text.ASCIIEncoding.Unicode.GetByteCount(_entiry.Message));
-
-                        try
-                        {
-                                if (_entiry.DefaultJsonParse)
-                                {
-                                        tbValue.Text = (string)JsonHelper.FormatJson(_entiry.Message);
-                                }
-                                else
-                                {
-                                        tbValue.Text = _entiry.Message;
-                                }
-                        }
-                        catch (Exception)
-                        {
-                                tbValue.Text = _entiry.Message;
-                        }
-
-                }
-
-                private void btnJson_Click(object sender, EventArgs e)
-                {
-                        try
-                        {
-                                tbValue.Text = (string)JsonHelper.FormatJson(_entiry.Message);
-                        }
-                        catch (Exception)
-                        {
-                                tbValue.Text = _entiry.Message;
-                                throw;
-                        }
-                }
-
-                protected override bool ProcessDialogKey(Keys keyData)
-                {
-                        if (Form.ModifierKeys == Keys.None && keyData == Keys.Escape)
-                        {
-                                this.Close();
-                                return true;
-                        }
-                        return base.ProcessDialogKey(keyData);
-                }
-
-                private void btnTextView_Click(object sender, EventArgs e)
-                {
-                        tbValue.Text = _entiry.Message;
-                }
+            InitializeComponent();
         }
+
+
+        private void MessageDetailForm_Load(object sender, EventArgs e)
+        {
+            tbId.Text = _entiry.Id.ToString();
+            tbTopic.Text = _entiry.Topic.ToString();
+            tbKey.Text = _entiry.KeyString;
+            lbSize.Text = RowFormatter.FormatSize(System.Text.ASCIIEncoding.Unicode.GetByteCount(_entiry.Message));
+
+            try
+            {
+                if (_entiry.DefaultJsonParse)
+                {
+                    tbValue.Text = (string)JsonHelper.FormatJson(_entiry.Message);
+                }
+                else
+                {
+                    tbValue.Text = _entiry.Message;
+                }
+            }
+            catch (Exception)
+            {
+                tbValue.Text = _entiry.Message;
+            }
+
+        }
+
+        private void btnJson_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tbValue.Text = (string)JsonHelper.FormatJson(_entiry.Message);
+            }
+            catch (Exception)
+            {
+                tbValue.Text = _entiry.Message;
+                throw;
+            }
+        }
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (Form.ModifierKeys == Keys.None && keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+            return base.ProcessDialogKey(keyData);
+        }
+
+        private void btnTextView_Click(object sender, EventArgs e)
+        {
+            tbValue.Text = _entiry.Message;
+        }
+    }
 }
--- KafkaHelpers/Helpers/JsonHelper.cs
+++ KafkaHelpers/Helpers/JsonHelper.cs
@@ -9,6 +9,7 @@ namespace KafkaHelpers
     public class JsonHelper
     {
         private const string INDENT_STRING = "    ";
+
         public static string FormatJson(string str)
         {
             var indent = 0;
@@ -51,6 +52,64 @@ namespace KafkaHelpers
                         if (!quoted)
                         {
                             sb.AppendLine();
+                            Enumerable.Range(0, indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        break;
+                    case ':':
+                        sb.Append(ch);
+                        if (!quoted)
+                            sb.Append(" ");
+                        break;
+                    default:
+                        sb.Append(ch);
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static string FormatJsonText(string str)
+        {
+            var indent = 0;
+            var quoted = false;
+            var sb = new StringBuilder();
+            for (var i = 0; i < str.Length; i++)
+            {
+                var ch = str[i];
+                switch (ch)
+                {
+                    case '{':
+                    case '[':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.Append(Environment.NewLine);
+                            Enumerable.Range(0, ++indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        break;
+                    case '}':
+                    case ']':
+                        if (!quoted)
+                        {
+                            sb.Append(Environment.NewLine);
+                            Enumerable.Range(0, --indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        sb.Append(ch);
+                        break;
+                    case '"':
+                        sb.Append(ch);
+                        bool escaped = false;
+                        var index = i;
+                        while (index > 0 && str[--index] == '\\')
+                            escaped = !escaped;
+                        if (!escaped)
+                            quoted = !quoted;
+                        break;
+                    case ',':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.Append(Environment.NewLine);
                             Enumerable.Range(0, indent).ForEach(item => sb.Append(INDENT_STRING));
                         }
                         break;
